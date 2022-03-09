9622fe0bd5b0a51e83ed01eed81c61aa88c11e99
tyscurtis25 <tyscurtis@gmail.com>
2022-03-09 00:31:59 -0700

    Added the shopping cart icon!

---
+++ ATBooks/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ATBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATBooks.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private ShoppingCart cart;
+        public CartSummaryViewComponent(ShoppingCart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
